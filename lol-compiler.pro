c8a540624b5d9f96ac7cbb60c9f9c8654eba0b95
Kupchinsky <user@killer666.ru>
2015-10-20 19:37:13 +0700

    - Initial

---
+++ lol-compiler.pro
@@ -0,0 +1,18 @@
+#-------------------------------------------------
+#
+# Project created by QtCreator 2015-10-20T18:31:00
+#
+#-------------------------------------------------
+
+QT       += core
+
+QT       -= gui
+
+TARGET = lol-compiler
+CONFIG   += console
+CONFIG   -= app_bundle
+
+TEMPLATE = app
+
+
+SOURCES += main.cpp
---
+++ main.cpp
@@ -0,0 +1,284 @@
+#include <QDebug>
+#include <QString>
+#include <QVector>
+#include <QPair>
+#include <QRegExp>
+#include <QFile>
+
+/*
+ * 8. Входной язык содержит операторы цикла for (…; …; …) do …, разделённые символом ; (точка с запятой).
+ * Операторы цикла содержат идентификаторы, знаки сравнения <, >, =, шестнадцатеричные числа, знак присваивания (:=).
+ * Шестнадцатеричными числами считать последовательность цифр и симво-лов a, b, c, d, e, f, начинающуюся с цифры (например, 89, 45ac, 0abc).
+*/
+
+class ILexicalExpression
+{
+public:
+    virtual QString getType() const = 0;
+    virtual bool isThis(QString) = 0;
+};
+
+// Операторы цикла
+class LE_ForOperator : public ILexicalExpression
+{
+    QString getType() const
+    {
+        return "Cycle initial operator";
+    }
+
+    bool isThis(QString input)
+    {
+        return input == "for";
+    }
+};
+
+class LE_DoOperator : public ILexicalExpression
+{
+    QString getType() const
+    {
+        return "Cycle begin operator";
+    }
+
+    bool isThis(QString input)
+    {
+        return input == "do";
+    }
+};
+
+class LE_CompareLessThanOperator : public ILexicalExpression
+{
+    QString getType() const
+    {
+        return "Compare less than operator";
+    }
+
+    bool isThis(QString input)
+    {
+        return input == "<";
+    }
+};
+
+class LE_CompareGreaterThanOperator : public ILexicalExpression
+{
+    QString getType() const
+    {
+        return "Compare greater than operator";
+    }
+
+    bool isThis(QString input)
+    {
+        return input == ">";
+    }
+};
+
+class LE_CompareEqualsOperator : public ILexicalExpression
+{
+    QString getType() const
+    {
+        return "Compare equals operator";
+    }
+
+    bool isThis(QString input)
+    {
+        return input == "=";
+    }
+};
+
+class LE_AssignOperator : public ILexicalExpression
+{
+    QString getType() const
+    {
+        return "Assign operator";
+    }
+
+    bool isThis(QString input)
+    {
+        return input == ":=";
+    }
+};
+//
+
+// Числа
+class LE_NumberConstant : public ILexicalExpression
+{
+    QString getType() const
+    {
+        return "Number constant";
+    }
+
+    bool isThis(QString input)
+    {
+        QRegExp pattern("^[0-9]+$");
+        return pattern.indexIn(input) != -1;
+    }
+};
+
+class LE_HexNumberConstant : public ILexicalExpression
+{
+    QString getType() const
+    {
+        return "Hex number constant";
+    }
+
+    bool isThis(QString input)
+    {
+        QRegExp pattern("^0x[0-9a-zA-Z]+$");
+        return pattern.indexIn(input) != -1;
+    }
+};
+//
+
+// Служебные символы (скобочки, точка с запятой)
+class LE_OpenBracketKeyWord : public ILexicalExpression
+{
+    QString getType() const
+    {
+        return "Open bracket key word";
+    }
+
+    bool isThis(QString input)
+    {
+        return input == "(";
+    }
+};
+
+class LE_CloseBracketKeyWord : public ILexicalExpression
+{
+    QString getType() const
+    {
+        return "Close bracket key word";
+    }
+
+    bool isThis(QString input)
+    {
+        return input == ")";
+    }
+};
+
+class LE_OpenSquareKeyWord : public ILexicalExpression
+{
+    QString getType() const
+    {
+        return "Open square bracket key word";
+    }
+
+    bool isThis(QString input)
+    {
+        return input == "{";
+    }
+};
+
+class LE_CloseSquareBracketKeyWord : public ILexicalExpression
+{
+    QString getType() const
+    {
+        return "Close square bracket key word";
+    }
+
+    bool isThis(QString input)
+    {
+        return input == "}";
+    }
+};
+//
+
+class LE_UnknownWord : public ILexicalExpression
+{
+    QString getType() const
+    {
+        return "Unknown word";
+    }
+
+    bool isThis(QString)
+    {
+        return true;
+    }
+};
+
+QVector<ILexicalExpression*> validExpressions;
+QVector<QPair<QString, ILexicalExpression*> > parsedExpressions;
+
+void pushValidExpressions()
+{
+    validExpressions.push_back(new LE_ForOperator());
+    validExpressions.push_back(new LE_DoOperator());
+    validExpressions.push_back(new LE_CompareLessThanOperator());
+    validExpressions.push_back(new LE_CompareGreaterThanOperator());
+    validExpressions.push_back(new LE_CompareEqualsOperator());
+    validExpressions.push_back(new LE_AssignOperator());
+    validExpressions.push_back(new LE_NumberConstant());
+    validExpressions.push_back(new LE_HexNumberConstant());
+    validExpressions.push_back(new LE_OpenBracketKeyWord());
+    validExpressions.push_back(new LE_CloseBracketKeyWord());
+    validExpressions.push_back(new LE_OpenSquareKeyWord());
+    validExpressions.push_back(new LE_CloseSquareBracketKeyWord());
+    validExpressions.push_back(new LE_UnknownWord());
+}
+
+void checkWord(QString input)
+{
+    QVectorIterator<ILexicalExpression*> iterator(validExpressions);
+
+    while (iterator.hasNext())
+    {
+        ILexicalExpression* expression = iterator.next();
+
+        if (expression->isThis(input))
+        {
+            QPair<QString, ILexicalExpression*> pair(input, expression);
+            parsedExpressions.push_back(pair);
+
+            break;
+        }
+    }
+}
+
+#define Delimiter ";"
+
+int main() // int argc, char *argv[]
+{
+    pushValidExpressions();
+
+    QFile file("input.txt");
+    if(!file.open(QIODevice::ReadOnly))
+    {
+        qDebug() << "Open file error:" << file.errorString();
+        return 1;
+    }
+
+    QTextStream inStream(&file);
+
+    while (!inStream.atEnd())
+    {
+        foreach (const QString &str, inStream.readLine().split(" "))
+        {
+            if (str.length() == 0)
+                continue;
+
+            if (str.indexOf(Delimiter) != -1)
+            {
+                foreach (const QString &str2, str.split(Delimiter))
+                {
+                    if (str2.length() != 0)
+                         checkWord(str2);
+                }
+            }
+            else
+                checkWord(str);
+        }
+    }
+
+    file.close();
+
+    qDebug() << "Parse complete. Expressions found: " << parsedExpressions.size();
+
+    QVectorIterator<QPair<QString, ILexicalExpression*> > iterator(parsedExpressions);
+
+    while (iterator.hasNext())
+    {
+        QPair<QString, ILexicalExpression*> pair = iterator.next();
+        qDebug() << "Word: " << pair.first << ", detected type: " << pair.second->getType();
+    }
+
+    return 0;
+}
